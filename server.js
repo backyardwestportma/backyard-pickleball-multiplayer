382719a59e45b70c3c10d33efcd624c6de3dd4aa
backyardwestportma <info@backyardwestport.com>
2026-05-05 20:35:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,30 +8,35 @@ app.use(express.static("public"));
 const rooms = {};
 
 io.on("connection", (socket) => {
-  socket.on("createGame", () => {
-    const roomCode = Math.floor(1000 + Math.random() * 9000).toString();
-    rooms[roomCode] = { players: [socket.id] };
-    socket.join(roomCode);
-    socket.emit("gameCreated", roomCode);
+
+  socket.on("createGame", (name) => {
+    const code = Math.floor(1000 + Math.random() * 9000).toString();
+
+    rooms[code] = {
+      players: [{ id: socket.id, name }],
+      ball: { x: 200, y: 300, vx: 4, vy: 3 },
+      score: [0, 0]
+    };
+
+    socket.join(code);
+    socket.emit("gameCreated", code);
   });
 
-  socket.on("joinGame", (code) => {
+  socket.on("joinGame", ({ code, name }) => {
     const room = rooms[code];
+
     if (room && room.players.length === 1) {
-      room.players.push(socket.id);
+      room.players.push({ id: socket.id, name });
       socket.join(code);
 
-      io.to(code).emit("startGame");
-    } else {
-      socket.emit("errorMessage", "Invalid or full room");
+      io.to(code).emit("startGame", room.players);
     }
   });
 
-  socket.on("paddleMove", ({ code, y }) => {
-    socket.to(code).emit("opponentMove", y);
+  socket.on("update", ({ code, state }) => {
+    socket.to(code).emit("update", state);
   });
+
 });
 
-http.listen(process.env.PORT || 3000, () => {
-  console.log("Server running");
-});
+http.listen(process.env.PORT || 3000);
